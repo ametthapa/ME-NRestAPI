555bb17968442b90af5a5ff2c2e2adb59eb20ca2
mohamad.ashish <mohamadashish786@gmail.com>
2021-12-22 17:58:40 +0545

    model created
    post request doing

---
+++ routes/subscribers.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Subscriber = require('../models/subscriberMode')
+
+//Getting all
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+//Getting one
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+
+})
+//Creating one
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscriberToChannel: req.body.subscriberToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+//Updating one
+router.patch('/', (req, res) => {
+
+})
+
+//Deleting one
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 
 const app = express()
 
 const mongoose = require('mongoose');
 const db = require('./utility/db');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const subscriberRoutes = require('./routes/subscribers');
+app.use('/subscribers', subscriberRoutes);
 
 
+
+// for database connection and to check port connection
 db.then(() => {
     app.listen(process.env.port, "0.0.0.0", () => {
         console.log("connected to the port");
